1ac6f12f9844cc82ff26ac1ccde336e890c6c3c7
filipe <spindolafilipe@gmail.com>
2017-11-27 19:33:34 -0200

    WIP

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,3 @@ from django.contrib import admin
 
 # Register your models here.
 from profile.models import Profile
-
-admin.site.register(Profile)
--- profile/admin.py
+++ profile/admin.py
@@ -1,24 +1,30 @@
-# from django.contrib import admin
-#
-# from profile.models import Profile, Settings
-#
-# class SettingsInline(admin.StackedInline):
-#     model = Settings
-#
-# class ProfileAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'name',
-#         'user',
-#         'location',
-#         'created',
-#         'updated',
-#         'last_login',
-#     )
-#     ordering = ('-user__last_login',)
-#
-#     inlines = (SettingsInline,)
-#
-#     def last_login(self, profile):
-#         return profile.user.last_login
-#
-# admin.site.register(Profile, ProfileAdmin)
+from django.contrib import admin
+
+from profile.models import Profile, Settings
+
+
+class SettingsInline(admin.StackedInline):
+    model = Settings
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'user',
+        'location',
+        'created',
+        'updated',
+        'last_login',
+    )
+
+    search_fields = ('user__username', 'name')
+
+    ordering = ('-user__last_login',)
+
+    inlines = (SettingsInline,)
+
+    def last_login(self, profile):
+        return profile.user.last_login
+
+
+admin.site.register(Profile, ProfileAdmin)
