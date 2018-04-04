6d3012a1f57c99096cca438092aa798d35dd0ca7
panorange <man.freemailer@gmail.com>
2018-04-04 17:20:57 +0300

    lazy loading styles

--- static/res/js/Views.js
+++ static/res/js/Views.js
@@ -76,46 +76,7 @@ class ProductListViews extends Views {
     }
 
     template(data){
-        return `
-            <div class="col-12 col-sm-6 col-md-12 col-lg-6 col-xl-4 col-xxl-3" id="div_listing_${data.profile_username}">
-
-              <div class="post-box">
-
-                <div class="post-cover listing-modal" style="background-image: url('${data.product_image}');" data-listing-id="${data.listing_id}">
-                  <span class="type">${data.listing_type}</span>
-                  <span class="price">${data.price} VH</span>
-
-                </div>
-
-                <div class="post-content listing-modal" data-listing-id="${data.listing_id}">
-                  <div class="avatar-thumbnail" style="background-image: url('${data.profile_image}');">
-                    <!--<img class="trusted-icon" src="/static/new_template/res/img/icons/trusted-icon.png"-->
-                         <!--alt="trusted">-->
-                  </div>
-
-                  <h1>${data.title}</h1>
-                  <p>${data.description}</p>
-                  <div class="tag-container">
-                      ${data['tags'].map(item => `
-                          <a class="tag" href="#">${item.name}</a>
-                      `)}
-                  </div>
-
-                </div>
-
-                <div class="post-action">
-
-                  <a class="action-menu-item trust-modal" data-profile-username="${data.profile_username}" href="/trust/${data.profile_username}"><img src="/static/new_template/res/img/icons/heart.png" alt="like"></a>
-                  <a class="action-menu-item payment-modal" data-profile-username="${data.profile_username}"><img src="/static/new_template/res/img/icons/wallet.png" alt="buy"></a>
-                  <a class="action-menu-item contact-modal" data-profile-username="${data.profile_username}"
-                     data-listing-title="${data.title}" href="#"><img src="/static/new_template/res/img/icons/mail.png" alt="contact"></a>
-
-                </div>
-
-              </div>
-
-            </div>
-        `
+        return data ? data : ''
     }
 }
 
